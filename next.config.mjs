2a34ff04cb8da98ad22d213b0cee176aefd87a68
yasiruranathunga <79520949+yasiruranathunga@users.noreply.github.com>
2024-09-26 21:36:45 +0530

    netlify form feature completed !!!

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,4 @@
-//After running next build, Next.js will produce an out folder which contains the HTML/CSS/JS assets for your application.
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
-/**
- * @type {import('next').NextConfig}
- */
-const nextConfig = {
-    output: 'export',
-   
-    // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-    // trailingSlash: true,
-   
-    // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-    // skipTrailingSlashRedirect: true,
-   
-    // Optional: Change the output directory `out` -> `dist`
-    // distDir: 'dist',
-  }
-   
-  module.exports = nextConfig
+export default nextConfig;
