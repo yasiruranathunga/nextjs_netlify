ec115431245befc62d1dcd3200d39cee60372801
yasiruranathunga <79520949+yasiruranathunga@users.noreply.github.com>
2024-09-30 13:28:08 +0530

    gg

--- next.config.js
+++ next.config.js
@@ -2,8 +2,15 @@
 const nextConfig = {
   output: 'export',
   images: {
-    unoptimized: true, // Disable Image Optimization
+    unoptimized: true, // Disable Image Optimization for static export
   },
 };
 
-module.exports = nextConfig;
+module.exports = {
+  ...nextConfig, // Spread the nextConfig object
+  env: {
+    CONTENTFULL_SPACE_ID: process.env.CONTENTFULL_SPACE_ID,
+    CONTENTFULL_ACCESS_TOKEN: process.env.CONTENTFULL_ACCESS_TOKEN,
+    ENTRY_ID: process.env.ENTRY_ID,
+  },
+};
