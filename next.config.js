2a24c3d911b0a6fd13a3a8f2e671ab138d2ffa0d
yasiruranathunga <79520949+yasiruranathunga@users.noreply.github.com>
2024-09-30 13:33:57 +0530

    gg

--- next.config.js
+++ next.config.js
@@ -2,15 +2,8 @@
 const nextConfig = {
   output: 'export',
   images: {
-    unoptimized: true, // Disable Image Optimization for static export
+    unoptimized: true, // Disable Image Optimization
   },
 };
 
-module.exports = {
-  ...nextConfig, // Spread the nextConfig object
-  env: {
-    CONTENTFULL_SPACE_ID: process.env.CONTENTFULL_SPACE_ID,
-    CONTENTFULL_ACCESS_TOKEN: process.env.CONTENTFULL_ACCESS_TOKEN,
-    ENTRY_ID: process.env.ENTRY_ID,
-  },
-};
+module.exports = nextConfig;
